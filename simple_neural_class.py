46b463b718eb68852243ac6b6b8b0b8f6488d415
Evan Meade <evanm831@gmail.com>
2019-10-01 21:25:20 -0500

    Began adding simple neural network from scratch

---
+++ simple_neural_class.py
@@ -0,0 +1,49 @@
+'''
+simple_neural_class.py
+
+Basic classification neural network built from scratch.
+
+Built with reference to Konstantinos Kitsios' article on Medium:
+https://towardsdatascience.com/how-to-build-a-simple-neural-network-from-scratch-with-python-9f011896d2f3
+
+'''
+
+import numpy as np
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(z))
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    W1 = np.random.randn(n_h, n_x)
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h)
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {
+        "W1": W1,
+        "b1": b1,
+        "W2": W2,
+        "b2": b2
+    }
+
+    return parameters
+
+
+def forward_prop(X, parameters):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+
+    cache = {
+        "A1": A1,
+        "A2": A2
+    }
+    return A2, cache
