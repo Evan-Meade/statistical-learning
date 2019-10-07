d4ed995254ae5e75ff32b9af64c033092a559b6f
Evan Meade <evan.meade@protonmail.com>
2019-10-07 11:47:17 -0500

    Began commenting neural

--- simple_neural_class.py
+++ simple_neural_class.py
@@ -4,24 +4,51 @@ Evan Meade, 2019
 
 Basic classification neural network built from scratch.
 
+Neural networks are a class of machine learning algorithms modelled loosely
+off of the human brain. They are designed to detect subtle patterns through
+supervised learning and a refinement of model parameters. They can be used
+predictively for both classification and regression.
+
+Execution Format:
+python simple_neural_class.py
+
 Built with reference to Konstantinos Kitsios' article on Medium:
 https://towardsdatascience.com/how-to-build-a-simple-neural-network-from-scratch-with-python-9f011896d2f3
+https://gitlab.com/kitsiosk/xor-neural-net
 
 '''
 
+# External package imports
 import numpy as np
 
 
+'''
+sigmoid(z)
+
+Returns normalized value of z with formula sig = 1/(1+e^(-z))
+'''
 def sigmoid(z):
-    return 1 / (1 + np.exp(z))
+    return 1 / (1 + np.exp(-z))
 
 
+'''
+initialize_parameters(n_x, n_h, n_y)
+
+Creates initial parameter matrices with random values.
+
+The weight matrices are initialized with random values, but the bias matrices
+are initialized as zero matrices. Each function parameter represents the
+number of nodes at each layer: n_x is the input layer, n_h is the hidden
+layer, and n_y is the output layer.
+'''
 def initialize_parameters(n_x, n_h, n_y):
+    # Initializing matrices
     W1 = np.random.randn(n_h, n_x)
     b1 = np.zeros((n_h, 1))
     W2 = np.random.randn(n_y, n_h)
     b2 = np.zeros((n_y, 1))
 
+    # Creating dictionary of parameter matrices
     parameters = {
         "W1": W1,
         "b1": b1,
@@ -29,24 +56,42 @@ def initialize_parameters(n_x, n_h, n_y):
         "b2": b2
     }
 
+    # Returning parameter dictionary
     return parameters
 
 
+'''
+forward_prop(X, parameters)
+
+Carries out algorithm to calculate output layer with current parameters.
+
+Executes each layer with appropriate normalization and returns the normalized
+layers.
+'''
 def forward_prop(X, parameters):
+    # Reading in parameters
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
 
+    # Calculates hidden layer values
     Z1 = np.dot(W1, X) + b1
+    # Normalizes hidden layer values
     A1 = np.tanh(Z1)
+
+    # Calculates output layer values
     Z2 = np.dot(W2, A1) + b2
+    # Normalizes output layer values
     A2 = sigmoid(Z2)
 
+    # Creates dictionary of normalized layers
     cache = {
         "A1": A1,
         "A2": A2
     }
+
+    # Returns normalized layers
     return A2, cache
 
 
@@ -135,6 +180,8 @@ def predict(X, parameters):
 
     return y_predict
 
+
+np.random.seed(2)
 
 X = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])
 
